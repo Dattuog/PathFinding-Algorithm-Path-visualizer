5c749e6bf747e055fece08320f0df9e6f98e89f3
Dattuog <prsds10082003@gmail.com>
2024-08-24 21:26:18 +0530

    updated readme

--- src/components/AppNavbar/AppNavbar.js
+++ src/components/AppNavbar/AppNavbar.js
@@ -32,7 +32,7 @@ const AppNavbar = (props) => {
         <Collapse isOpen={isOpen} navbar>
           <Nav  navbar>
             <NavItem id={"Tooltip-" + 6}>
-              <NavLink href="https://github.com/chakshu71/PathFinding-Algorithm-Path-visualizer.git" target='_blank'>GitHub</NavLink>
+              <NavLink href="https://github.com/Dattuog/PathFinding-Algorithm-Path-visualizer.git" target='_blank'>GitHub</NavLink>
             </NavItem>
             <UncontrolledDropdown nav inNavbar>
               <DropdownToggle id={"Tooltip-" + 5} nav caret>
